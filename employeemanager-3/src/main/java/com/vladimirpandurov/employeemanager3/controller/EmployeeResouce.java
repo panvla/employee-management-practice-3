c21c9d0f422dea0b976e7c749c7cd1c14e33818c
panvla <pandvlado@gmail.com>
2022-11-12 11:36:08 +0100

    added bunch of stuff

---
+++ employeemanager-3/src/main/java/com/vladimirpandurov/employeemanager3/controller/EmployeeResouce.java
@@ -0,0 +1,5 @@
+package com.vladimirpandurov.employeemanager3.controller;
+
+
+public class EmployeeResouce {
+}
---
+++ employeemanager-3/src/main/java/com/vladimirpandurov/employeemanager3/exception/UserNotFoundException.java
@@ -0,0 +1,6 @@
+package com.vladimirpandurov.employeemanager3.exception;
+
+public class UserNotFoundException extends X {
+    public UserNotFoundException(String s) {
+    }
+}
